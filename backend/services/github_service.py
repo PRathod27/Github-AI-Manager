c28fec390e5037ea05c4f9de2849476ddc3bca3f
Prem Rathod <prem.rathod.2715@gmail.com>
2026-04-15 00:26:18 +0530

    testing diff fix

--- backend/services/github_service.py
+++ backend/services/github_service.py
@@ -8,18 +8,27 @@ headers = {
 
 def get_commit_diff(repo: str, commit_sha: str):
     url = f"https://api.github.com/repos/{repo}/commits/{commit_sha}"
-    
-    response = requests.get(url, headers=headers)
-    data = response.json()
 
-    # Combine all file patches
-    diff = ""
-    for file in data.get("files", []):
-        if "patch" in file:
-            diff += file["patch"] + "\n"
+    headers_diff = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3.diff"   # 🔥 KEY CHANGE
+    }
+
+    response = requests.get(url, headers=headers_diff)
+
+    print("Status:", response.status_code)
+
+    if response.status_code != 200:
+        print("❌ Error:", response.text)
+        return ""
+
+    diff = response.text
+
+    print("📏 Raw diff length:", len(diff))
+    print("📄 Diff preview:\n", diff[:200])
 
     return diff
-
+    
 def create_issue(repo: str, title: str, body: str):
     url = f"{BASE_URL}/repos/{repo}/issues"
 
