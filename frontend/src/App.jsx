0913b51d9f2ffc417e15686606f736bf9b15a40e
Prem Rathod <prem.rathod.2715@gmail.com>
2026-04-25 02:42:10 +0530

    Auth Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import AuthSuccess from "./components/AuthSuccess";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/auth-success" element={<AuthSuccess />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
